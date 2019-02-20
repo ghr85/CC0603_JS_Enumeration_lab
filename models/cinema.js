008347f6cb33cb69511d20d430747c154cb8071b
ghr85 <ghrenfrew@gmail.com>
2019-02-20 14:14:13 +0000

    get titles function added to cinema model

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,9 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.get_titles = function(){
+  return this.films.map(function(film){
+    return film.title;
+  });
+};
 module.exports = Cinema;
